f78b4d288872e5d6d72e52d30508ae6048e1bf66
andreasx23 <49310598+andreasx23@users.noreply.github.com>
2023-02-01 21:56:10 +0100

    Removed unused imports

--- src/main/java/com/GuildSpamFilter/Handlers/CollectionLogHandler.java
+++ src/main/java/com/GuildSpamFilter/Handlers/CollectionLogHandler.java
@@ -5,8 +5,6 @@ import com.GuildSpamFilter.Models.Section;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class CollectionLogHandler
 {
